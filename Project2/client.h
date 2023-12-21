f2854b30be22481cd2318f466769acc7430fddbe
Afonso Neves <up202108884@up.pt>
2023-12-21 17:26:51 +0000

    version 1.1

--- Project2/client.h
+++ Project2/client.h
@@ -1,21 +1,32 @@
-#include <netdb.h>
 #include <stdio.h>
+#include <netdb.h>
+#include <regex.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <termios.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
 /* FTP SETTINGS */
 #define FTP_PORT 21
-#define SERVER_PORT 6000
-#define SERVER_ADDR "192.168.28.96"
 
 /* LOGIN CREDENTIALS */
 #define RCOM "rcom"
 #define DEFAULT_USER "anonymous"
 #define DEFAULT_PASS "password"
+
+/* REGULAR EXPRESSIONS */
+#define AT              "@"
+#define BAR             "/"
+#define HOST_REGEX      "%*[^/]//%[^/]"
+#define HOST_AT_REGEX   "%*[^/]//%*[^@]@%[^/]"
+#define RESOURCE_REGEX  "%*[^/]//%*[^/]/%s"
+#define USER_REGEX      "%*[^/]//%[^:/]"
+#define PASS_REGEX      "%*[^/]//%*[^:]:%[^@\n$]"
+#define RESPCODE_REGEX  "%d"
+#define PASSIVE_REGEX   "%*[^(](%d,%d,%d,%d,%d,%d)%*[^\n$)]"
 
 /* SERVER RESPONSES */
 #define READY_AUTH 210
@@ -26,18 +37,18 @@
 #define TRANSFER_COMPLETE 226
 #define GOODBYE 221
 
-/* URL PARAMETERS LENGTH */
+/* LENGTHS */
 #define MAX_LENGTH 300
-#define URL_PARAM_LENGTH 100
+#define URL_LENGTH 100
 
 typedef struct
 {
-    char ip[URL_PARAM_LENGTH];
-    char host[URL_PARAM_LENGTH];
-    char user[URL_PARAM_LENGTH];
-    char pass[URL_PARAM_LENGTH];
-    char resource[URL_PARAM_LENGTH];
-    char file[URL_PARAM_LENGTH];
+    char ip[URL_LENGTH];
+    char host[URL_LENGTH];
+    char user[URL_LENGTH];
+    char pass[URL_LENGTH];
+    char resource[URL_LENGTH];
+    char file[URL_LENGTH];
 } URL;
 
 typedef enum
@@ -48,8 +59,9 @@ typedef enum
     END
 } ResponseState;
 
-/*
-int parse(char *input, URL *url);
+int getIP(char *hostname, struct hostent **h);
+
+int parseToURL(char *input, URL *url);
 
 int createSocket(char *ip, int port);
 
@@ -64,4 +76,3 @@ int requestResource(const int socket, char *resource);
 int getResource(const int socketA, const int socketB, char *filename);
 
 int closeConnection(const int socketA, const int socketB);
-*/
